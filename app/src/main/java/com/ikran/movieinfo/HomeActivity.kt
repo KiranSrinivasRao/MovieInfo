e05c8266b8461551acbec79cdeb347491e8d7e0b
kiransrinivasrao <kiran.srinivasrao@icloud.com>
2020-08-15 19:06:39 +0530

    Base commit - Initialised Hilt, AndroidX dependencies in gradle

---
+++ app/src/main/java/com/ikran/movieinfo/HomeActivity.kt
@@ -0,0 +1,8 @@
+package com.ikran.movieinfo
+
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity: BaseActivity() {
+
+}
---
+++ app/src/main/java/com/ikran/movieinfo/MovieInfoApp.kt
@@ -0,0 +1,9 @@
+package com.ikran.movieinfo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieInfoApp : Application() {
+
+}
